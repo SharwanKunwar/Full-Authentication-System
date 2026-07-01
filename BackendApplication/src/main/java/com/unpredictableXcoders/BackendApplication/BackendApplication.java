c20c81e379397bc33e2af0983b3fd54506222e5c
SharwanKunwar <sharwankunwar07@gmail.com>
2026-07-01 12:41:57 +0545

    added Dotenv setup

--- BackendApplication/src/main/java/com/unpredictableXcoders/BackendApplication/BackendApplication.java
+++ BackendApplication/src/main/java/com/unpredictableXcoders/BackendApplication/BackendApplication.java
@@ -1,5 +1,6 @@
 package com.unpredictableXcoders.BackendApplication;
 
+import io.github.cdimascio.dotenv.Dotenv;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
@@ -8,6 +9,8 @@ public class BackendApplication {
 
 	public static void main(String[] args)
 	{
+		Dotenv dotenv = Dotenv.configure().load();
+		dotenv.entries().forEach((entry -> System.setProperty(entry.getKey(), entry.getValue())));
 		SpringApplication.run(BackendApplication.class, args);
 	}
 
